6403e44d4dd3f82dadb461f6813c4b2a54963228
Kourosh Mirzaei <kouroshmirzaei@Kouroshs-MBP.fios-router.home>
2019-01-01 04:04:14 -0500

    Updates

---
+++ precache-manifest.72941624a145ecef20fef63a003578cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5ca2be5f8ea22e3e363",
+    "url": "/react-web-api/static/js/runtime~main.d5ca2be5.js"
+  },
+  {
+    "revision": "69388e3bc8d558abea67",
+    "url": "/react-web-api/static/js/main.69388e3b.chunk.js"
+  },
+  {
+    "revision": "e6f73a97f7441bd2c6fa",
+    "url": "/react-web-api/static/js/1.e6f73a97.chunk.js"
+  },
+  {
+    "revision": "69388e3bc8d558abea67",
+    "url": "/react-web-api/static/css/main.342d99f6.chunk.css"
+  },
+  {
+    "revision": "742f3b99a7930772697487dc86c0dfb7",
+    "url": "/react-web-api/index.html"
+  }
+];
